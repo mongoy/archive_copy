68b4c06fba8ec5e66b2a7d6fe782e469c74ba128
mongoy <mongoy@rambler.ru>
2021-08-13 17:01:16 +0900

    archive

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import os
 import zipfile
 import datetime
+import time
 
 
 def check_dir(path_dir):
@@ -27,31 +28,50 @@ def check_dir(path_dir):
                   datetime.datetime.fromtimestamp(int(os.path.getmtime(path_dir))))
             status = 2
         elif os.path.isdir(path_dir):
-            print('КАТАЛОГ')
-            print('Список объектов в нем: ', os.listdir(path_dir))
+            print(f'КАТАЛОГ: {path_dir}')
+            # print('Список объектов в нем: ', os.listdir(path_dir))
             status = 1
     else:
         print('Объект не найден')
     return status
 
 
-def archive_directory():
-    base_dir = os.path.abspath("")
+def archive_directory(src):
+    base_dir = os.path.abspath(src)
     with zipfile.ZipFile("qqq.zip", "w") as zf:
-        pass
+        base_path = os.path.split(src)[0]
+        # print(base_path)
+        for root, dirs, files in os.walk(src):
+            # print(root, dirs, files)
+            for file in files:
+                zf.write(os.path.join(root, file))
+                # print(dirs)
+    return
 
 
-def copy_zip_file():
-    pass
+def move_zip_file(file, path):
+    os.replace(file, path)
 
 
 def main():
+    start_time = time.monotonic()
     # наличие файла конфигурации
     if check_dir("config.json") == 2:
         with open("config.json", "r", encoding="utf-8") as conf:
             text = json.load(conf)  # dict
             src, dst = text.values()  #
             print(src, dst)
+        # наличие директорий
+        if (check_dir(src) == 1) and (check_dir(dst) == 1):
+            # создание архива
+            # archive_directory(src)
+            # print(f"Время архивации - {time.monotonic() - start_time} сек.")
+            # # перенос архива
+            move_zip_file("qqq.zip", os.path.join(os.getcwd(), dst))
+        else:
+            print(f"Проверьте наличие директорий: {src, dst}")
+    else:
+        print("Проверьте наличие файла конфигурации - config.json")
 
     # archive_directory()
     # copy_zip_file()
